aea33b4906fa91ee68dfe97da1b94f0109ea0ca7
jonbrooks <jonbrooks@users.noreply.github.com>
2022-05-23 10:33:00 -0700

    Add SPM support (#18)

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "mParticle-Leanplum",
+    platforms: [ .iOS(.v9) ],
+    products: [
+        .library(
+            name: "mParticle-Leanplum",
+            targets: ["mParticle-Leanplum"]),
+    ],
+    dependencies: [
+      .package(name: "mParticle-Apple-SDK",
+               url: "https://github.com/mParticle/mparticle-apple-sdk",
+               .upToNextMajor(from: "8.7.0")),
+      .package(name: "Leanplum",
+               url: "https://github.com/leanplum/leanplum-ios-sdk",
+               .upToNextMajor(from: "3.1.0")),
+    ],
+    targets: [
+        .target(
+            name: "mParticle-Leanplum",
+            dependencies: ["mParticle-Apple-SDK","Leanplum"],
+            path: "mParticle-Leanplum",
+            publicHeadersPath: "."),
+    ]
+)
